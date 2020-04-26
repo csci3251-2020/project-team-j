e5aab60a8f5b42e0693b715ebeb92c6b610ec46b
kelvinwongbs <61689241+kelvinwongbs@users.noreply.github.com>
2020-04-26 08:26:08 +0800

    Update code.c

--- code.c
+++ code.c
@@ -1,1 +1,31 @@
-// Your C code here
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int test1_positive = rand() % 2;
+    int test2_positive = rand() % 2;
+    int test3_positive = rand() % 2;
+    if (test1_positive) {
+        if (test2_positive) {
+            printf("Unfortunately! You are confirmed with COVID-19.");
+        } else {
+            if (test3_positive) {
+                printf("Unfortunately! You are confirmed with COVID-19.");
+            } else {
+                printf("Congratulations! You are not infected!");
+            }
+        }
+    } else {
+        if (test2_positive) {
+            if (test3_positive) {
+                printf("Unfortunately! You are confirmed with COVID-19.");
+            } else {
+                printf("Congratulations! You are not infected!");
+            }
+        } else {
+            printf("Congratulations! You are not infected!");
+        }
+    }
+
+    return 0;
+}
